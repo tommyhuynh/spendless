2f325560803499c818fc735a1c63e6dc7571bbb4
James Jiang <jmendre@berkeley.edu>
2017-06-18 22:00:27 -0700

    week fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,11 @@ class HomeController < ApplicationController
   	last_week_expenses = Expense.where(:created_at => 1.week.ago.beginning_of_week..1.week.ago.end_of_week)
   	this_week_expenses = Expense.where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week)
     @category_data = category.map{|c| [c.name,c.expenses.where(gains:false).sum("amount")]}.to_h
+    @last_week_expenses = Expense.where(gains:false).where(:created_at => Time.now.last_week.beginning_of_week..Time.now.end_of_week)
+    @this_week_expenses = Expense.where(gains:false).where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week)
+
     @last_week_category_data = category.map{|c| [c.name,c.expenses.
-    	where(gains:false).where(:created_at => 1.week.ago.beginning_of_week..1.week.ago.end_of_week).
+    	where(gains:false).where(:created_at => Time.now.last_week.beginning_of_week..Time.now.last_week.end_of_week).
     	sum("amount")]}.to_h
     @this_week_category_data = category.map{|c| [c.name,c.expenses.
     	where(gains:false).where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week).

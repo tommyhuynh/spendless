4e388261edb7e5112deebae38ce73f87d0e7bc3e
Tommy Huynh <tommyhuynh@berkeley.edu>
2017-06-18 20:27:35 -0700

    mobile fix?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
 
   def graphs
   	category = Category.all
-    @category_data = category.map{|c| [c.name,c.expenses.where(gains:false).sum("amount")]}.to_h
+    @category_data_this_week = category.map{|c| [c.name,c.expenses.where(gains:false, :created_at => Time.now.beginning_of_week..Time.now).sum("amount")]}.to_h
+    @category_data_last_week = category.map{|c| [c.name,c.expenses.where(gains:false, :created_at => 1.week.ago.beginning_of_week..1.week.ago.end_of_week).sum("amount")]}.to_h
     @daily_expense_data = Expense.where(gains:false).group_by_day(:created_at).sum("amount")
   end
 end

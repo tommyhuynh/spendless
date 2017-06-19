e4bd591f14c109e30e3a5507c92588338783aad3
James Jiang <jmendre@berkeley.edu>
2017-06-18 17:57:28 -0700

    this week

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,21 @@ class HomeController < ApplicationController
 
   def graphs
   	category = Category.all
+  	last_week_expenses = Expense.where(:created_at => 1.week.ago.beginning_of_week..1.week.ago.end_of_week)
+  	this_week_expenses = Expense.where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week)
     @category_data = category.map{|c| [c.name,c.expenses.where(gains:false).sum("amount")]}.to_h
+    @last_week_category_data = category.map{|c| [c.name,c.expenses.
+    	where(gains:false).where(:created_at => 1.week.ago.beginning_of_week..1.week.ago.end_of_week).
+    	sum("amount")]}.to_h
+    @this_week_category_data = category.map{|c| [c.name,c.expenses.
+    	where(gains:false).where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week).
+    	sum("amount")]}.to_h
+
     @daily_expense_data = Expense.where(gains:false).group_by_day(:created_at).sum("amount")
+    @weekly_expense_data = Expense.where(gains:false).group_by_week(:created_at).sum("amount")
+    @monthly_expense_data = Expense.where(gains:false).group_by_month(:created_at).sum("amount")
+
+
+
   end
 end

354a4b27670483b890b9076144500929e3885244
James Jiang <jmendre@berkeley.edu>
2017-06-22 19:26:13 -0700

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,9 @@ class HomeController < ApplicationController
     @last_week_expenses = Expense.where(gains:false).where(:created_at => Time.now.last_week.beginning_of_week..Time.now.end_of_week)
     @this_week_expenses = Expense.where(gains:false).where(:created_at => Time.now.beginning_of_week..Time.now.end_of_week)
 
+    @all_time_category_data = category.map{|c| [c.name,c.expenses.
+    	where(gains:false).
+    	sum("amount")]}.to_h
     @last_week_category_data = category.map{|c| [c.name,c.expenses.
     	where(gains:false).where(:created_at => Time.now.last_week.beginning_of_week..Time.now.last_week.end_of_week).
     	sum("amount")]}.to_h
@@ -21,6 +24,7 @@ class HomeController < ApplicationController
     @weekly_expense_data = Expense.where(gains:false).group_by_week(:created_at).sum("amount")
     @monthly_expense_data = Expense.where(gains:false).group_by_month(:created_at).sum("amount")
 
+	@category_multiseries = category.map {|category| {name: category.name, data: category.expenses.group_by_day(:created_at).sum("amount")}}
 
 
   end
